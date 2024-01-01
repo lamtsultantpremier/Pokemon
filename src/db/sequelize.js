0bc8c78f81e030a0b0762bf509aa93f1c28e2e0d
lamtsultantpremier <laminekone7818@gmail.com>
2024-01-01 09:29:42 +0100

    new

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD || "",
   {
-    host: process.env.DB_NAME,
+    host: "localhost",
     dialect: "mariadb",
     logging: false,
     port: process.env.DB_PORT || 3306,
